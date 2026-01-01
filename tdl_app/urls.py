1d50ff252dd2e3a30c6e24aaf7543238cd5dbda6
bipinjlimbu <bipinlimbu712@gmail.com>
2026-01-01 14:00:31 +0545

    Routing Done

---
+++ tdl_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views.auth_view import login_view, register_view
+from .views.main_view import index_view
+
+urlpatterns = [
+    path('',index_view,name='index'),
+    path('/login',login_view,name='login'),
+    path('/register',register_view,name='register'),
+]
