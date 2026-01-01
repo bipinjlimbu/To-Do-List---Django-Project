e4135d5958f538466853b4189da548ae3a2652a7
bipinjlimbu <bipinlimbu712@gmail.com>
2026-01-01 14:27:43 +0545

    DB Connected

--- tdl_project/settings.py
+++ tdl_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'tdl_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todolist_django',
+        'HOST' : 'localhost',
+        'USER' : 'root',
+        'PASSWORD' : '',
+        'PORT' : 3306,
     }
 }
 
