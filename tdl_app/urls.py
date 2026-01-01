b27ba6ecc29dea152ee18294a66839fd536b0c48
bipinjlimbu <bipinlimbu712@gmail.com>
2026-01-01 14:04:47 +0545

    Setup Done

--- tdl_app/urls.py
+++ tdl_app/urls.py
@@ -4,6 +4,6 @@ from .views.main_view import index_view
 
 urlpatterns = [
     path('',index_view,name='index'),
-    path('/login',login_view,name='login'),
-    path('/register',register_view,name='register'),
+    path('login/',login_view,name='login'),
+    path('register/',register_view,name='register'),
 ]
