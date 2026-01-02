00af0667e307d87d3ddd91503a891b0f4d7602fe
bipinjlimbu <bipinlimbu712@gmail.com>
2026-01-02 06:35:31 +0545

    Urls for Logout Added

--- tdl_app/urls.py
+++ tdl_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views.auth_view import login_view, register_view
+from .views.auth_view import login_view, register_view, logout_view
 from .views.main_view import index_view
 
 urlpatterns = [
     path('',index_view,name='index'),
     path('login/',login_view,name='login'),
     path('register/',register_view,name='register'),
+    path('logout/',logout_view,name='logout')
 ]
