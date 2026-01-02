3e9b41783ad3d8f49c23cd0e0dc549d4e109e329
bipinjlimbu <bipinlimbu712@gmail.com>
2026-01-02 07:09:20 +0545

    Add Task Page and View Added

--- tdl_app/urls.py
+++ tdl_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views.auth_view import login_view, register_view, logout_view
-from .views.main_view import index_view
+from .views.main_view import index_view, add_task
 
 urlpatterns = [
     path('',index_view,name='index'),
     path('login/',login_view,name='login'),
     path('register/',register_view,name='register'),
-    path('logout/',logout_view,name='logout')
+    path('logout/',logout_view,name='logout'),
+    path('add_task/',add_task,name='add_task')
 ]
--- tdl_app/views/main_view.py
+++ tdl_app/views/main_view.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index_view(request):
     return render(request,'main/index.html')
+
+def add_task(request):
+    return render(request,'main/add_task.html')
