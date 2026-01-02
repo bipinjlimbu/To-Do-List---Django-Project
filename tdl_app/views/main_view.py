7b9fe90ce4371a40c0e69b99d1e8ff1c756da556
bipinjlimbu <bipinlimbu712@gmail.com>
2026-01-02 07:45:54 +0545

    Add Task View Added

--- tdl_app/views/main_view.py
+++ tdl_app/views/main_view.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from ..models import todolist
+from django.contrib import messages
 
 def index_view(request):
     return render(request,'main/index.html')
 
 def add_task(request):
+    errors = {}
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+
+        if not title:
+            errors['title'] = "Please Add Task Title."
+        elif len(title) < 5 or len(title) > 50:
+            errors['title'] = 'Title Cannot Less than 5 or More than 50.'
+
+        if not description:
+            errors['description'] = 'Please Add Description of Your Task.'
+        
+        if errors:
+            return render(request, 'main/add_task.html', {'errors': errors, 'data': request.POST})
+        
+        else:
+            tdl = todolist(
+                title = title,
+                description = description,
+                user = request.user,
+            ) 
+            tdl.save()
+            messages.success(request,'Task Added Successfully.')
+            return redirect('index')
+        
     return render(request,'main/add_task.html')
